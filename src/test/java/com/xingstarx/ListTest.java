637e0c8896c964f6f171bcbdb4ae711afb3cf242
xingxing <xxx823952375@gmail.com>
2016-10-26 23:03:11 +0800

    使用junit4

--- src/test/java/com/xingstarx/ListTest.java
+++ src/test/java/com/xingstarx/ListTest.java
@@ -1,37 +1,15 @@
 package com.xingstarx;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
-public class ListTest
-        extends TestCase {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public ListTest(String testName) {
-        super(testName);
-    }
+import org.junit.Test;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite() {
-        return new TestSuite(ListTest.class);
-    }
+public class ListTest{
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp() {
-        assertTrue(true);
-    }
 
     /**
      * 测试单链表结构，并打印结果
      */
+    @Test
     public void testList() {
         int[] str = new int[]{1, 2, 3, 4, 5, 6};
         List list = new List(str);
